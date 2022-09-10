428e3212e0f2cecf05884baea1b8c21b6546b04e
MustafaYavas <mustafayavas40@gmail.com>
2022-09-10 19:02:33 +0300

    Create project structure and navbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,24 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import {  Route, Routes } from 'react-router-dom';
+import { Container } from 'react-bootstrap';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import Home from './pages/Home';
+import Store from './pages/Store';
+import About from './pages/About';
+import Navbar from './components/UI/Navbar';
+
+const App = () => {
+    return (
+        <>
+            <Navbar />
+            <Container className='mb-5'>
+                <Routes>
+                    <Route path='/' element={<Home />}/>
+                    <Route path='/store' element={<Store />}/>
+                    <Route path='/about' element={<About />}/>
+                </Routes>
+            </Container>
+        </>
+    )
 }
 
 export default App;
